ff2e65a136b70b559946badb40a9d1c5e09873df
pravinraajhan <192521066.simats@saveetha.com>
2026-06-16 03:49:18 -0500

    Add files via upload

---
+++ forward.pl
@@ -0,0 +1,17 @@
+% Facts
+has_wings.
+cannot_fly.
+
+% Rules
+can_fly :-
+    has_wings.
+
+bird(penguin) :-
+    cannot_fly.
+
+bird(eagle) :-
+    can_fly.
+
+% Forward chaining rule
+infer(X) :-
+    bird(X).
